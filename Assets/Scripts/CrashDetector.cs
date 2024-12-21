db06cf0f8f51e7cbebf38ef994914ab7811fe86a
Praneethk-18 <siri46546@gmail.com>
2024-12-20 20:49:43 -0600

    Snowboarder_subTasks

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -7,11 +7,16 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+
+    bool hasCrashed = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Ground")
+        if (collision.tag == "Ground" && !hasCrashed)
         {
+            hasCrashed = true;
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", loadDelay);
         }
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float torqueAmount;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
+
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2d;
 
     // Start is called before the first frame update
     void Start()
     {
        rb2d = GetComponent<Rigidbody2D>(); 
+       surfaceEffector2d = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+
+    }
+
+    void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -25,6 +37,21 @@ public class PlayerController : MonoBehaviour
         {
             rb2d.AddTorque(-torqueAmount);
         }
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            surfaceEffector2d.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2d.speed = baseSpeed;
+        }
+        //if We push up, then speed up
+        // Otherwise stay at normal speed
+        // access the surface  effector2d and change its speed
 
     }
 }
